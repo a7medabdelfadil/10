7bc4f86678fe672fd891b2b315e9950bb763c6cb
a7medabdelfadil <a7medfadel7@gmail.com>
2024-03-17 17:13:06 +0200

    update

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-function validateForm(event) {
-    event.preventDefault();
-    
-    const username = document.getElementById('username').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
+function validateForm(e) {
+    e.preventDefault();
+
+    let 
+    username = document.getElementById('username').value.trim(),
+    email = document.getElementById('email').value.trim(),
+    password = document.getElementById('password').value.trim();
 
     if (!validateUsername(username)) {
-        alert('the password should have Should have 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character and be at least 8 characters long.');
+        alert('username should have a length of 3 to 16 characters')
         return;
     }
-
+    
     if (!validateEmail(email)) {
-        alert('Please enter a valid email address.');
+        alert('please enter a valid email')
         return;
     }
-
-    if (!validatePass(password)) {
-        alert('the password should have Should have 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character and be at least 8 characters long.');
+    
+    if (!validatePassword(password)) {
+        alert('Should have 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character and be at least 8 characters long')
         return;
     }
-
-    alert('Form submitted successfully!');
-    document.getElementById('contactForm').reset();
+    alert('form submitted successfully');
+    document.getElementById('form').reset();
 }
 
 function validateUsername(username) {
-    const usernameRegex = /^[a-z0-9_-]{3,16}$/;
+    let usernameRegex = /^[a-z0-9_-]{3,16}$/;
     return usernameRegex.test(username)
 }
 
 function validateEmail(email) {
-    const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10})*$/;
-    return emailRegex.test(email);
+    let emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10})*$/;
+    return emailRegex.test(email)
 }
 
-function validatePass(pass) {
-    const passRegex = /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/;
-    return passRegex.test(pass)
+function validatePassword(password) {
+    let passwordRegex = /(?=(.*[0-9]))(?=.*[\!@#$%^&*()\\[\]{}\-_+=~`|:;"'<>,./?])(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{8,}/;
+    return passwordRegex.test(password)
 }
